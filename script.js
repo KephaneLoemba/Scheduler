f24f0755a748e2e059beb6c95450f14493985d5a
dentyne icebourg <kloem058@gmail.com>
2020-03-17 17:14:00 -0400

    Fixed code that generates our schedule, in javascript. Will arrange the layout in future commit.

--- script.js
+++ script.js
@@ -5,24 +5,62 @@ $(document).ready(function(){
     
     
     //Let's create some arrays, containing ou different timeslots/key names.
-    let timeSlots = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM',]
+    let timeSlots = ['9AM', '10AM']
+
+    //'11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM',
+
     
     
-    timeSlots.forEach(function(time) {
+    
+    for (i = 0; i < timeSlots.length; i++) {
+
+        let hour = 9;
         
-        let time = $('<>')
+        let newRow = $('<div>');
+        newRow.addClass('row plannerRow');
+        newRow.text(timeSlots[i]);
+        newRow.attr('hour-index', hour);
         
-        letterBtn.addClass('row letter letter-button-color')
+
+        let timeCol = $('<div>');
+        timeCol.addClass('col-md-2');
+
+        let timeColContent = $('<p>');
+
+        timeColContent.attr('placeholder', 'Nothing planned yet, for this hour.');
+        timeColContent.html(timeSlots[i]);
+
+        timeCol.append(timeColContent)
+
+        let inputCol = $('<div>');
+        inputCol.addClass('col-md-8');
+
+        let inputColContent = $('<input>');
+        inputColContent.attr('type', 'text');
+        inputCol.append(inputColContent);
+
+        let saveCol = $('<div>')
+        saveCol.addClass('col-md-2')
+
+        let saveBtn = $('<button>')
+        saveBtn.attr('save-index', hour)
+        saveBtn.html("<i class='far fa-save'></i>")
+        saveCol.append(saveBtn)
+
+        $('div#bigContainer').append(newRow);
+        newRow.append(timeCol);
+        newRow.append(inputCol);
+
+        hour++;
+
+        // let timeSave = $('<button>')
+
+        // timeSave.addClass('saveBtn').text(timeSlots[i])
         
-        // 4. Then give each "letterBtn" a data-attribute called "data-letter".
-        letterBtn.attr('data-letter', letters[i])
-        // 5. Then give each "letterBtns" a text equal to "letters[i]".
-        letterBtn.text(letters[i])
-        // <button class="letter-button letter letter-button-color" data-letter="A">A</button>
-        
-        // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
-        $('#buttons').append(letterBtn)
-    })
+        // $('#bigContainer').append(timeSave)
+
+
+    }
 
     //End of code!
 })
