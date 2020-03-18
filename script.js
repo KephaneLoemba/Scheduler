6e0fc8df2eb3a9989e9b7cfb5f47d8b3fceb5d70
dentyne icebourg <kloem058@gmail.com>
2020-03-18 17:38:05 -0400

    Rows now correctly change color, based on the current time.

--- script.js
+++ script.js
@@ -27,26 +27,25 @@ $(document).ready(function(){
 
     
     //Dynamically create all of the calendar's rows.
+    let hour = 9;
 
     for (i = 0; i < timeSlots.length; i++) {
 
-        let hour = 9;
         
         let newRow = $('<div>');
         newRow.addClass('row plannerRow');
-        newRow.attr('hour-index', hour);
         
-
         let timeCol = $('<div>');
         timeCol.addClass('col-md-1');
-
+        
         let timeColContent = $('<p>');
         timeColContent.html(timeSlots[i]);
-
+        
         timeCol.append(timeColContent)
-
+        
         let inputCol = $('<div>');
-        inputCol.addClass('col-md');
+        inputCol.addClass('col-md row-colors');
+        inputCol.attr('hour-index', hour);
 
         let inputColContent = $('<textarea>');
         inputColContent.attr('placeholder', 'Nothing planned yet, for this hour.');
@@ -64,11 +63,51 @@ $(document).ready(function(){
         newRow.append(timeCol);
         newRow.append(inputCol);
         newRow.append(saveCol);
-
+        
+      
+        
         hour++;
-
+        
 
     }
+
+    //Introduce the currentHour variable, using moment.js. this hour will be marked in the 24h format.
+
+    let currentHour = moment().format('H');
+    currentHour = parseInt(currentHour);
+
+    
+    //add code that dynamically chages the text area's CSS pase on what time it is. Run this every second.
+
+    setInterval( function() {
+        updateRowColor()
+    },1000);
+
+    function updateRowColor() {
+        thisRow = $('.row-colors').toArray();
+        console.log(thisRow)
+
+        thisRow.forEach(function(timeSlot) {      
+        
+            
+                hour = $(timeSlot).attr('hour-index');
+                hour = parseInt(hour);
+
+                console.log(typeof hour)
+                console.log(typeof currentHour)
+
+            if (hour < currentHour) {
+                $(timeSlot).removeClass('present future').addClass('past');
+
+            } else if (hour > currentHour) {
+                $(timeSlot).removeClass('past present').addClass('future');
+
+            } else {
+                $(timeSlot).removeClass('past future').addClass('present');
+            }
+        })
+    }
+
 
     //End of code!
 })
